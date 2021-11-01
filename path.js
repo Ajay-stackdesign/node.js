42ab1b99905a133e179806de75c898e4038a949f
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-01 23:49:54 +0530

    changes

---
+++ path.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+console.log(path.dirname("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(path.extname("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(path.basename("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(path.isAbsolute("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(path.normalize("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(path.parse("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+console.log(split(path.sep))//("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/path.js"))
+
