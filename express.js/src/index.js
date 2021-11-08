220f99856f4dab1a8688ffa137b8f68c7fb20677
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-08 23:01:56 +0530

    changes

--- express.js/src/index.js
+++ express.js/src/index.js
@@ -5,25 +5,39 @@
 const path = require("path")
 const express = require("express")
 const app = express();
+const hbs = require("hbs")
 
 
 const staticPath = path.join(__dirname,"../public");
+const templatePath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials")
 
 // builtin middleware
 
+// app.use(express.static(staticPath));
+app.set('view engine', "hbs")
+app.set("views",templatePath);
+hbs.registerPartials(partialsPath);   
+
 app.use(express.static(staticPath));
 
+app.get("", (req,res) => {
+    res.render("index", {
+        channelName : "ajay",
+    });
+}) 
+
 app.get("/",(req,res) => {
-    res.send("hello your express respose is send1")
+    res.send("hello your express respose is send1");
 })
 
-const pathing = path.join(__dirname,"../about")
+// const pathing = path.join(__dirname,"../about")
 
-app.use(express.static(pathing));
+// app.use(express.static(pathing));
 
-app.get("/about",(req,res) => {
-    res.send("hello express")
-})
+// app.get("/about",(req,res) => {
+//     res.send("hello express")
+// })
 
 app.listen(8000,"127.0.0.1",() => {
     console.log("hello your response is end 8000 ")
