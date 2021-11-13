c44a61f58b71c1d139e8892c7de5f9f75cd566cd
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-13 23:33:39 +0530

    changes

--- expresspractise/myapp/src/index.js
+++ expresspractise/myapp/src/index.js
@@ -15,7 +15,7 @@ app.set("views",templatePath); //changing the folder in hbs
 hbs.registerPartials(partialPath);
 
 app.get("/",(req,res) => {
-    res.render('C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/templates/views/index.hbs',{
+    res.render('C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/myapp/templates/views/index.hbs',{
         channelName : "ajay"
     });//dynamic way to add a data;
 })
@@ -27,25 +27,39 @@ app.get("/",(req,res) => {
 })
 
 app.get("/about",(req,res) => {
-    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/templates/views/about.hbs")
+    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/myapp/templates/views/about.hbs")
 })//dynamic ways to add a data
-
 app.get("/contact",(req,res) => {
-    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/templates/viewscontact.hbs")
+    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/myapp/templates/views/contact.hbs")
 })
 
-app.get("/about",(req,res) => {
-    res.send("hello here is your about us page")
-})
-app.get("/contact",(req,res) => {
-    res.send("hello here is your contact page")
-})
+// app.get("/about",(req,res) => {
+//     res.send("hello here is your about us page")
+// })
+
+// app.get("/contact",(req,res) => {
+//     res.send("hello here is your contact page")
+// })
+
 app.get("/temp",(req,res) => {
     res.send({
         id: 1,
         name: "ajay"
     })
 })
+
+app.get("/about/*",(req,res) => {
+    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/myapp/templates/views.404.hbs",{
+        errorcomment: "opp this about us page could not not be found",
+    })
+})
+
+app.get("*",(req,res) => {
+    res.render("C:/Users/Ajay Sahni/Desktop/AjayGIt/node.js/node1.js/expresspractise/myapp/templates/views/404.hbs",{
+        errorcomment: "page not found",
+    })
+})
+
 
 app.listen(8080,"127.0.0.1",() => {
     console.log("listening your response on 8080 port")
