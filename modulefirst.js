072b3457f6181593f0a178961840fec525e34617
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-21 23:33:31 +0530

    changes

---
+++ modulefirst.js
@@ -0,0 +1,10 @@
+
+// import simple from "./modeseconds.mjs";
+
+// // const package = require("./modeseconds.mjs")
+// // const module = require("./modeseconds.js")
+
+// simple();
+import { simple } from "./modeseconds";
+
+simple();
