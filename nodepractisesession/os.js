0cc5928ff9a4718eef7a1c49f8380ff69c0a792f
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-21 23:28:50 +0530

    changes

--- nodepractisesession/os.js
+++ nodepractisesession/os.js
@@ -1,13 +1,13 @@
-const { PRIORITY_ABOVE_NORMAL } = require("constants");
+// const { PRIORITY_ABOVE_NORMAL } = require("constants");
 const os = require("os")
 
-// console.log(os.EOL)
+console.log(os.EOL)
 // console.log(os.constants)
 // console.log(os.arch())
 // console.log(os.cpus())
 // console.log(os.devNull)
 // console.log(os.endianness())
-// // console.log(os.freemem())
+// console.log(os.freemem())
 // const freemem = os.freemem()
 // console.log(`${freemem/1024/1024/1024}`)
 // console.log(os.getPriority())
@@ -16,10 +16,10 @@ const os = require("os")
 // console.log(os.loadavg())
 // console.log(os.version())
 // console.log(os.uptime());
-console.log(os.type());
+// console.log(os.type());
 // console.log(os.totalmem())
 // console.log(os.tmpdir())
-// // console.log(os.setPriority())
+// console.log(os.setPriority())
 // console.log(os.release())
 // console.log(os.platform())
 // console.log(os.networkInterfaces())
