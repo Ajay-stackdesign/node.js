1c3f2990c1aece3d59c91cdf6545a3a937ecefc7
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-21 23:31:43 +0530

    changes

---
+++ modeseconds.js
@@ -0,0 +1,9 @@
+// export const simple = () => {
+//     console.log("simple is simple")
+// }
+
+// module.exports = simple ;
+// export default simple
+export default function simple(){
+    console.log("simple is wimple")
+}
